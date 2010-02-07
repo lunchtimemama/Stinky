8b7f17b16cc7657d0ab67027b1c6eb612a3dcd3b
Scott Thomas <scott@scott-laptop.(none)>
2010-02-07 17:09:57 -0500

    Column width formatting fix.

--- MonoDevelop.Stinky/MonoDevelop.Stinky/EvaluationWidget.cs
+++ MonoDevelop.Stinky/MonoDevelop.Stinky/EvaluationWidget.cs
@@ -49,7 +49,8 @@ namespace MonoDevelop.Stinky
 					value.Visit(interpreter);
 				}
 			});
-			var rootTokenizer = new Driver((indentation, expression) => expression.Visit(resolver), new ErrorConsumer(null, null));
+			var rootTokenizer = new Driver(
+				(indentation, expression) => expression.Visit(resolver), new ErrorConsumer(null, null));
 			var scrolledWindow = new ScrolledWindow();
 			readEvalPrintLoopView = new ReadEvalPrintLoopView(text => {
 				var column = 0;
